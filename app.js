e1f89d09f9500b6ad6314af3a57762980584c105
Piotr Grzywna <Piotr.Grzywna@pl.ibm.com>
2023-11-24 14:24:25 +0100

    send and receive message between server and client

--- app.js
+++ app.js
@@ -17,6 +17,25 @@ const port = 8080;
 // connection to public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Create 1st connection, by edit app js in  - by opening up connection,
+//   using io variable and specifying an event called as connection.
+io.on('connection', (socket) => {
+	// adding socket argument - used each tome we want to send and receive the message
+	console.log(`new connection was made`);
+
+	// use emit in on method as our primary method of handling events in socket.io
+	// passing a string event-listener, and JSON object - sending greeting message send from server
+	socket.emit('message-from-server'),
+		{
+			greeting: 'Hello from server',
+		};
+	// second event-listener that will listen for message from client
+	// passing a string event-listener, and function that will grab message from client, and display in log message
+	socket.listen('message-from-client', (msg) => {
+		console.log(`message-from-client: ${msg}`);
+	});
+});
+
 // Listening tp our server on defined port
 server.listen(port, () => {
 	console.log(`Listening on port: http://localhost:${port}`);
